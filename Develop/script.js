c5e4b54c33d19ebc9b154b0b8b715f7e55e97270
Gerry Paul <gpaul12@nc.rr.com>
2023-02-11 12:35:06 -0500

    next commit

--- Develop/script.js
+++ Develop/script.js
@@ -10,6 +10,10 @@ number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 space = [];
 
+var choices;
+var toUpper = function (x) {
+  return x.toUpperCase();
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
