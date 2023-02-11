22499e139b8000f94c5bc9bd54b326c642c4183f
Gerry Paul <gpaul12@nc.rr.com>
2023-02-11 12:32:37 -0500

    next commit

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,12 @@ var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
 
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+space = [];
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
